d7496513b094cd0d9ca57b4056e1f0f906440ef2
ProklB <proklb@ya.ru>
2025-04-14 10:59:58 +0500

    Спринт8 Тема2 Урок5 ДЗ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.hfad.playlistmaker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hfad.playlistmaker"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
