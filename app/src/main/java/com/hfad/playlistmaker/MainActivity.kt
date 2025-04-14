0aa5bb9e02e0afecfb57e76398e59b9368499ce9
ProklB <proklb@ya.ru>
2025-04-14 11:50:51 +0500

    Спринт8 Тема4 Урок2 ДЗ

--- app/src/main/java/com/hfad/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/hfad/playlistmaker/MainActivity.kt
@@ -3,6 +3,8 @@ package com.hfad.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        button1.setOnClickListener(buttonClickListener)
+
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека!", Toast.LENGTH_SHORT).show()
+        }
+
+        val button3 = findViewById<Button>(R.id.button3)
+        button3.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
