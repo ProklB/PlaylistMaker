c1700ebcb3f639f8b05b7da7d910e318a1872868
ProklB <proklb@ya.ru>
2025-05-09 11:12:56 +0500

    С9Т3У3 ДЗ

--- app/src/main/java/com/hfad/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/hfad/playlistmaker/SearchActivity.kt
@@ -1,14 +1,54 @@
 package com.hfad.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+import android.widget.ImageButton
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.title)
+        setSupportActionBar(toolbar)
+
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearButton = findViewById< ImageButton>(R.id.clearButton)
+
+        // Фокус на поле ввода
+        searchEditText.requestFocus()
+
+        // TextWatcher для отслеживания изменений текста
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Не используется
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Показать, скрыть кнопку очистки в зависимости от наличия текста
+                clearButton.visibility = if (s.isNullOrEmpty()) android.view.View.GONE else android.view.View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Эаглушка для будущей логики поиска
+            }
+        })
+
+        clearButton.setOnClickListener {
+            // Очищаем текст
+            searchEditText.text.clear()
+            clearButton.visibility = android.view.View.GONE
+        }
+
     }
+
 }
