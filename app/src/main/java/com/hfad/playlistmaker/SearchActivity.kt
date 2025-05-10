7cce2123f688c1889e982399c79b9c652a9f2ea8
ProklB <proklb@ya.ru>
2025-05-10 20:06:25 +0500

    Спринт9 ДЗ

--- app/src/main/java/com/hfad/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/hfad/playlistmaker/SearchActivity.kt
@@ -1,15 +1,20 @@
 package com.hfad.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.EditText
 import android.widget.ImageButton
 import com.google.android.material.appbar.MaterialToolbar
+import android.view.inputmethod.InputMethodManager
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var searchEditText: EditText
+    private var currentText = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -21,34 +26,42 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        searchEditText = findViewById<EditText>(R.id.searchEditText)
         val clearButton = findViewById< ImageButton>(R.id.clearButton)
 
-        // Фокус на поле ввода
-        searchEditText.requestFocus()
-
-        // TextWatcher для отслеживания изменений текста
         searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // Не используется
+
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Показать, скрыть кнопку очистки в зависимости от наличия текста
                 clearButton.visibility = if (s.isNullOrEmpty()) android.view.View.GONE else android.view.View.VISIBLE
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // Эаглушка для будущей логики поиска
+                currentText = s.toString()
             }
         })
 
         clearButton.setOnClickListener {
-            // Очищаем текст
             searchEditText.text.clear()
             clearButton.visibility = android.view.View.GONE
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
         }
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("SAVED_SEARCH_TEXT", currentText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedText = savedInstanceState.getString("SAVED_SEARCH_TEXT", "")
+        searchEditText.setText(savedText)
+        currentText = savedText
     }
 
 }
